5f4d0cfa828756e11b740aa139c784fe7ecc5eb7
aashish46ak-lab <aashish46ak@gmail.com>
2026-08-11 00:36:46 +0545

    main: wire floating Install FAB globally

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,6 +14,7 @@ import { AuthProvider } from "@/hooks/useAuth";
 import { Toaster } from "@/components/ui/sonner";
 import { RoleRedirect } from "@/components/RoleRedirect";
 import { SplashScreen } from "@/components/SplashScreen";
+import { InstallFAB } from "@/components/InstallFAB";
 import { registerPWA } from "@/lib/pwa-register";
 
 function NotFoundComponent() {
@@ -170,6 +171,7 @@ function RootComponent() {
           )}
           <RoleRedirect />
           <Outlet />
+          <InstallFAB />
           <Toaster richColors position="top-right" />
         </ClientErrorBoundary>
       </AuthProvider>
